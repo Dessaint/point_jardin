b54712f9ece4e1f655e34d99229a50c07ca49d82
Dessaint <youtoun@hotmail.fr>
2016-03-22 09:53:00 +0100

    Création Link

--- homepage.php
+++ homepage.php
@@ -1,14 +1,34 @@
 <!DOCTYPE html>
-<html>
+<html lan="fr">
 	<head>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" type="text/css" href="style.css">
 
 	</head>
 	
 	<body>
+		<div class="container">
 <?php
 require (header.php)
 ?>
+<section>
+	<div class="cont_cat">
+		<div class="section_haut">
+			<a href="mineraux.php"><img src="" alt=""/></a>
+			<a href="accessoires.php"><img src="" alt=""/></a>
+		</div>
+		<div class="section_bas">
+			<a href="vegetaux.php"><img src="" alt=""/></a>
+			<a href="services.php"><img src="" alt=""/></a>
+		</div>
+	</div>
+</section>
 
+<?php
+require (footer.php)
+?>
+
+		</div>
 	</body>
 
 </html>
