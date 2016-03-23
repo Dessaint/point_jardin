6b4000d9539c16ae7a15706d9ac7a013ebbf4be7
etidimjav <thomas.javanaud@gmail.com>
2016-03-23 10:09:10 +0100

    Ajout header

---
+++ header.php
@@ -0,0 +1,17 @@
+<header>
+<div class="en_tete">
+	<div class="logo"></div>
+	<navbar>
+		<ul>
+			<li><a href="#">ACCUEIL</a></li>
+			<li><a href="#">NOS MINERAUX</a></li>
+			<li><a href="#">NOS VEGETAUX</a></li>
+			<li><a href="#">NOS ACCESSOIRES</a></li>
+			<li><a href="#">NOS SERVICES</a></li>
+			<li><a href="#">CONTACTS</a></li>
+		</ul>
+	</navbar>
+	<div class="reseaux_sociaux"></div>
+	<div class="image"></div>
+</div>
+</header>
